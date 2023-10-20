0d283339059cc00b03886cdb335865ee73591b94
Gwendeeth <xkreet@hotmail.com>
2023-10-19 20:56:00 -0400

    Creacion De la pagina

---
+++ script.js
@@ -0,0 +1,16 @@
+//alert("Hola este es mi Javascript");
+
+let nombre = "Wendy";
+nombre = "Juan";
+var nombre1 = "Wendy";
+
+const nombre2 = "Wendy";
+
+console.log(nombre)
+console.log(nombre1)
+console.log(nombre2)
+
+
+let contenidoTitulo = "Nombre";
+
+let titulo = document.querySelector(".logo .fuente")
